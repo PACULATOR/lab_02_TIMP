ac36d86e40f6ebd9623d9dfc0e18cea276d6e15f
pure_slander <post96slander@mail.ru>
2022-03-18 19:53:15 +0300

    remove_using std

---
+++ Hello_world.cpp
@@ -0,0 +1,11 @@
+int main(){
+#include <iostream>
+std::string a;
+std::cin>a;
+std::coun<< "hello world from" <<a <<"!"
+return 0;
+}
+EOF
+
+EOF
+
--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-using namespace std;
 int main() {
-    string a;
-    cin >> a;
-    cout << "Hello, World from" <<a <<endl;
+    std::string a;
+    std::cin >> a;
+    std::cout << "Hello, World from" <<a <<endl;
     return 0;
 }
