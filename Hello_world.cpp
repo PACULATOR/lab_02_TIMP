52e532e07c6e2f874384e8defa7745679106834b
PACULATOR <michailmatushk@mail.ru>
2022-03-24 21:24:50 +0300

    4enge_style_cleng_formet

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -1,11 +1,17 @@
-int main(){
+int
+main()
+{
 #include <iostream>
-std::string a;
-std::cin>a;
-std::coun<< "hello world from" <<a <<"!"
-return 0;
+  std::string a;
+  std::cin > a;
+  std::coun << "hello world from" << a << "!" return 0;
 }
 EOF
 
+<<<<<<< HEAD
 EOF
 //ыаыаыаыааыпыаыаыаааааааааууууууудадкщалвлчруоалвопдуьчтоа
+=======
+  EOF
+  // eto comment
+>>>>>>> 4enge_style_cleng_formet
